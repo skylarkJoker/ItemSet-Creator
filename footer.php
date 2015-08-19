b31b329836c20a2066a873312f59311ecdc295f8
Jo-Hann Joseph <skylarknash@gmail.com>
2015-08-19 01:07:17 -0400

    Revert "Changes"
    This reverts commit 5260851086afe4e72db9a498b79e31ca4b3c0d29.
    
    Conflicts:
    	load.php

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <footer>
-
+  <p>
     Copyright (c) 2015 Copyright Holder All Rights Reserved.
-
+  </p>
+  <p>
+        Item Set Creator isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+  </p>
 </footer>
 <script src='script.js'></script>
 </body>
